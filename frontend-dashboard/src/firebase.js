4a0fa7b0d97fa66ffe90fd0f0f4dbcfb5cddbccf
sk1ban312 <skybamykyta@gmail.com>
2025-10-19 19:06:54 -0400

    Update firebase.js

--- frontend-dashboard/src/firebase.js
+++ frontend-dashboard/src/firebase.js
@@ -1,14 +1,13 @@
 // File: frontend-dashboard/src/firebase.js
 import { initializeApp } from "firebase/app";
-// IMPORT the necessary functions from the correct module
 import { getDatabase, ref, onValue, query, limitToLast } from "firebase/database";
 
-// Your web app's Firebase configuration (from next.config.js env variables)
+// Your web app's Firebase configuration from Vercel environment variables
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 // Initialize Firebase
